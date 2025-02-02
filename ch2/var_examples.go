9f31c370afd56fdac4d18313309ab9029831659e
rgt <rasbin@outlook.com>
2025-02-02 11:14:34 +0545

    variables

---
+++ ch2/var_examples.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+func main(){
+    // var a int;
+    // var b = 20;
+    // c := 10
+    // fmt.Println("c old", c);
+    // var d string;
+    // var e rune;
+    //
+    // fmt.Println("a: ", a);
+    // fmt.Println("b: ", b);
+    // fmt.Println("c: ", c);
+    // fmt.Println("d: ", d);
+    // fmt.Println("e: ", e);
+    CMain();
+}
