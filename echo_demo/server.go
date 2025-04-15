b5869d2056f079b3f96093fe99c99c520fe8c33a
rasbin <rasbin.thapa@learnsic.com>
2025-04-15 18:15:31 +0545

    minor changes

--- echo_demo/server.go
+++ echo_demo/server.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func main(){
-    e := echo.New()
+func main() {
+	e := echo.New()
 
-    e.GET("/", func (c echo.Context) error {
-        return c.String(http.StatusOK, "Hello world and its awesome")
-    })
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world and its awesome")
+	})
 
-    e.POST("/users", func (c echo.Context) error{
-        return c.String(http.StatusOK, "Users created")
-    })
+	e.POST("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Users created")
+	})
 
-
-    e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323"))
 }
