9916d2c37f6700d3f61e6349e0ee6f5b2a5ac294
rasbin <rasbingodarthapa@gmail.com>
2025-04-29 13:27:20 +0545

    echo demo

--- echo_demo/server.go
+++ echo_demo/server.go
@@ -10,11 +10,11 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello world and its awesome")
+		return c.JSON(http.StatusOK, "Hello world and its awesome")
 	})
 
 	e.POST("/users", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Users created")
+		return c.JSON(http.StatusOK, "Users created")
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
